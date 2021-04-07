7625482c0b2dbb71ed909b859498d83f264c8248
Leontin <leontin>
2021-04-07 21:37:26 +0300

    Initial commit

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,10 @@
+package ro.java.ctrln;
+
+public class Main {
+
+    //JAR
+
+    public static void main(String[] args) {
+        System.out.println("Invat limbajul Java si despre masina virtuala Java!");
+    }
+}
