05a7751388c22d450b2b748cdf9ef7de652dbd1c
Leontin <leontin>
2021-04-08 12:32:14 +0300

    Modificare Main.Java

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,6 +3,7 @@ package ro.java.ctrln;
 public class Main {
 
     //JAR
+    //Alt comentariu
 
     public static void main(String[] args) {
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
